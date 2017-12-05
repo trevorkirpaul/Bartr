9bea16f08cad4490737485ec28729fbc2f9671a1
Trevor Kirpaul <tkirpaul@gmail.com>
2017-12-05 17:05:02 -0500

    ip for api now works, altho serving images is different

--- src/serverLocation.js
+++ src/serverLocation.js
@@ -1,29 +1,33 @@
-const ip = `http://165.227.111.10`;
+const ipUnsecure = `http://165.227.111.10`;
+
+// use this for https
+const ip = 'https://trevorkirpaul.com';
+
 // const ip = 'http://localhost';
 // const locadlHost = `http://ip`;
 
 
-export const URLimage = `${ip}:3001/avatar/`;
-export const URLimageItem = `${ip}:3001/`;
-export const APIitem = `${ip}:3001/api/item/`;
+export const URLimage = `${ipUnsecure}/avatar/`;
+export const URLimageItem = `${ipUnsecure}/`;
+export const APIitem = `${ip}/api/item/`;
 
 
 // used in items.js actions
-export const APIallItems = `${ip}:3001/api/items`;
+export const APIallItems = `${ip}/api/items`;
 
 // used while creating new item for sale, image
-export const APIitemImg = `${ip}:3001/api/items/img`;
+export const APIitemImg = `${ip}/api/items/img`;
 
 // used in actions login.js
-export const APIlogin = `${ip}:3001/api/login`;
+export const APIlogin = `${ip}/api/login`;
   // update account and avatarUpdate
-  export const APIaccount = `${ip}:3001/api/account`;
+  export const APIaccount = `${ip}/api/account`;
 
 // accounts actions
   // create
-  export const accountCreateAPI = `${ip}:3001/api/accountCreate`;
-  export const getAccountInfoAfterCreate = `${ip}:3001/api/account`;
+  export const accountCreateAPI = `${ip}/api/accountCreate`;
+  export const getAccountInfoAfterCreate = `${ip}/api/account`;
   // createAvatar
-  export const avatarCreateAPI = `${ip}:3001/api/accountCreate/avatar`;
+  export const avatarCreateAPI = `${ip}/api/accountCreate/avatar`;
   
   
